de4fdf24da4b914e3e18630926cdd121f6b42fff
Leslie <leslieleigh@outlook.com>
2018-07-27 16:53:53 +0800

    update skeleton animation tests-cases

--- examples/playground/src/skinning-animation.js
+++ examples/playground/src/skinning-animation.js
@@ -6,7 +6,10 @@
   let dobj = {
     baseUrl: '../assets/out',
     scene: 'spec-skeleton',
-    entityPath: 'skeleton'
+    entityPath: 'Hero',
+    animationclips: [],
+    movementSpeed : 0,
+    isHealth: true
   };
 
   dgui.remember(dobj);
@@ -15,104 +18,6 @@
   dgui.add(dobj, 'entityPath');
 
   load();
-
-  /*
-   * @param string id
-   * @param string text
-   * @return ButtonComponent
-  * */
-  function createButton(id, buttoncontent) {
-    let ent = app.createEntity(id);
-
-    let image = ent.addComp('Image');
-    image.setOffset(0, 0);
-    image.setAnchors(0.5, 0.5, 0.5, 0.5);
-    image.setSize(160, 30);
-
-    let entLabel = app.createEntity('label');
-    entLabel.setParent(ent);
-    let text = entLabel.addComp('Text');
-    text.setAnchors(0, 0, 1, 1);
-    text.setSize(0, 0);
-    text.text = buttoncontent;
-    text.color = color4.new(0, 0, 0, 1);
-    text.align = 'middle-center';
-
-    let button = ent.addComp('Button');
-    button.background = ent;
-    button.transition = 'color';
-    button.transitionColors.normal = color4.new(1, 1, 1, 1);
-    button.transitionColors.highlight = color4.new(0.7, 0.7, 0.7, 1);
-    button.transitionColors.pressed = color4.new(0.5, 0.5, 0.5, 1);
-    button.transitionColors.disabled = color4.new(0.2, 0.2, 0.2, 1);
-    button._updateState();
-
-    return ent;
-  }
-
-  /**
-   * @param {string} id
-   * @param {entity} screen
-   * @param {any} onValueChanged 
-   */
-  function createSlider(id, screen, onValueChanged) {
-    let rotation = quat.create();
-
-    let sliderEnt = app.createEntity(id);
-    sliderEnt.setParent(screen);
-    let sliderWidget = sliderEnt.addComp('Widget');
-    sliderWidget.setOffset(-100, 0);
-    sliderWidget.setSize(160, 20);
-    sliderEnt.setWorldRot(rotation);
-    let sliderComp = sliderEnt.addComp('Slider');
-    sliderComp.direction = 'horizontal';
-
-    let sliderBg = app.createEntity('bg');
-    sliderBg.setParent(sliderEnt);
-    let bgSprite = sliderBg.addComp('Image');
-    bgSprite.color = color4.new(1, 1, 1, 1);
-    bgSprite.setAnchors(0, 0, 1, 1);
-    bgSprite.setSize(0, 0);
-
-    let fillArea = app.createEntity('fillArea');
-    fillArea.setParent(sliderEnt);
-    let faWidget = fillArea.addComp('Widget');
-    faWidget.setAnchors(0, 0, 1, 1);
-    faWidget.setSize(-20, 0);
-    faWidget.setOffset(-5, 0);
-
-    let fill = app.createEntity('fill');
-    fill.setParent(fillArea);
-    let fillSprite = fill.addComp('Image');
-    fillSprite.color = color4.new(1, 0, 0, 1);
-    fillSprite.setAnchors(0, 0, 0, 1);
-    fillSprite.setSize(10, 0);
-
-    let handleArea = app.createEntity('handleArea');
-    handleArea.setParent(sliderEnt);
-    let haWidget = handleArea.addComp('Widget');
-    haWidget.setAnchors(0, 0, 1, 1);
-    haWidget.setSize(-20, 0);
-
-    let handle = app.createEntity('handle');
-    handle.setParent(handleArea);
-    let handleSprite = handle.addComp('Image');
-    handleSprite.color = color4.new(0, 1, 1, 1);
-    handleSprite.setAnchors(0, 0, 0, 1);
-    handleSprite.setSize(20, 20);
-    sliderComp.background = handle;
-    sliderComp.transition = 'color';
-    sliderComp.transitionColors.normal = color4.new(0, 1, 1, 1);
-    sliderComp.transitionColors.highlight = color4.new(1, 1, 0, 1);
-    sliderComp.transitionColors.pressed = color4.new(0.5, 0.5, 0.5, 1);
-    sliderComp.transitionColors.disabled = color4.new(0.2, 0.2, 0.2, 1);
-    sliderComp._updateState();
-
-    sliderComp.handle = handle;
-    sliderComp.fill = fill;
-
-    sliderEnt.on('Slider.onValueChanged', () => onValueChanged(sliderComp.value));
-  }
 
   function load() {
     resl({
@@ -132,28 +37,21 @@
           } else {
             app.loadLevel(level);
 
-            let sc = app.createEntity('screen');
-            sc.addComp('Screen');
-
             let mainEntity = app.find(dobj.entityPath);
             let mainEntityAnimation = mainEntity.getComp('Animation');
-            let iClip = 0;
-            for (let clip of mainEntityAnimation.clips) {
-              let clipName = clip.name;
-              console.log(clipName);
-              let button = createButton(`anim-button-${clipName}`, clipName);
-              button.setParent(sc);
-              button.getComp("Image").setOffset(800, 300 + -40 * (iClip++));
-              button.on("mousedown", () => {
-                mainEntityAnimation.play(clipName);
-              });
-            }
+
+            let clips = [];
+            for (let clip of mainEntityAnimation.clips)
+              clips.push(clip.name);
+            dgui.add(dobj, 'animationclips', clips).onFinishChange((value) => {
+              mainEntityAnimation.play(value);
+            });
 
             // The animation clips.
             let idleClip = mainEntityAnimation.getState("Idle").clip;
-            let walkClip = mainEntityAnimation.getState("Walk").clip;
-            let runClip = mainEntityAnimation.getState("Run").clip;
-            let deathClip = mainEntityAnimation.getState("Death").clip;
+            let walkClip = mainEntityAnimation.getState("WalkForward").clip;
+            let runClip = mainEntityAnimation.getState("RunForward").clip;
+            let deathClip = mainEntityAnimation.getState("ShootDown").clip;
 
             // Blend idle, Walk and run clip together to describe the character movement.
             // It's a blend tree.
@@ -185,22 +83,14 @@
             let reborn = deathMotion.makeTransitionTo(movementMotion);
             reborn.addCondition(new cc.animation.Condition(isHealth, 'equal', true));
 
-            let moveButton = createButton(`move-button`, "Move");
-            moveButton.setParent(sc);
-            moveButton.getComp("Image").setOffset(800, 300 + -40 * (iClip++));
-            moveButton.on("mousedown", () => {
-              mainEntityAnimation.animationGraph.linearSwitch(movementMotion);
+            mainEntityAnimation.animationGraph.linearSwitch(movementMotion);
+
+            dgui.add(dobj, "movementSpeed", 0.0, 2.0).onFinishChange((value) => {
+              blender.setInput(value);
             });
 
-            createSlider("slider", sc, (v) => {
-              blender.setInput(v * 2);
-            });
-
-            let healthButton = createButton(`health-button`, "Kill him");
-            healthButton.setParent(sc);
-            healthButton.getComp("Image").setOffset(800, 300 + -40 * (iClip++));
-            healthButton.on("mousedown", () => {
-              isHealth.value = !isHealth.value;
+            dgui.add(dobj, "isHealth", true).onFinishChange((value) => {
+              isHealth.value = value;
             });
           }
         });
